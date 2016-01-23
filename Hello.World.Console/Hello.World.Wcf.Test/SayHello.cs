7d142a794ba69f0d01cbe7f36a4723e5bc916f53
hecferme <hecferme@gmail.com>
2016-01-23 15:28:26 -0600

    Se corrige el wcf que apuntaba a la interfaz errónea y se crean pruebas automatizadas para HelloWorld

---
+++ Hello.World.Console/Hello.World.Wcf.Test/SayHello.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello.World.Wcf.Test.MyHelloWorldService;
+
+namespace Hello.World.Wcf.Test
+{
+    [TestClass]
+    public class SayHello
+    {
+        [TestMethod]
+        public void HelloDearUser()
+        {
+            // initialize
+            string userName = "Dear User";
+            string expectedResult = "Hello Dear User";
+            string realResult;
+            HelloWorldClient myService = new HelloWorldClient();
+
+            // test body
+            realResult = myService.SayHello(userName);
+
+            // assert
+            Assert.AreEqual(expectedResult, realResult);
+        }
+
+        [TestMethod]
+        public void HelloEmptyUser()
+        {
+            // initialize
+            string userName = "";
+            string expectedResult = "Hello ";
+            string realResult;
+            HelloWorldClient myService = new HelloWorldClient();
+
+            // test body
+            realResult = myService.SayHello(userName);
+
+            // assert
+            Assert.AreEqual(expectedResult, realResult);
+        }
+
+        [TestMethod]
+        public void HelloNull()
+        {
+            // initialize
+            string userName = null;
+            string expectedResult = "Hello ";
+            string realResult;
+            HelloWorldClient myService = new HelloWorldClient();
+
+            // test body
+            realResult = myService.SayHello(userName);
+
+            // assert
+            Assert.AreEqual(expectedResult, realResult);
+        }
+    }
+}
