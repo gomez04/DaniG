08f3b02fdfea4f8adfb9d986a5ed08fcaa30eb47
gomez04 <dani.gomez04@gmail.com>
2016-02-13 16:19:15 -0600

    Entity Framework

---
+++ Entity.Framework.Warming/Entity.Framework.Warming.Model/Modelo/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Framework.Warming.Model.Modelo
+{
+    public class Products : Product
+    {
+        public string MyProperty { get { return this.ListPrice.ToString(); } }
+        public int Antiquity { get { return ((int)((DateTime.Now - this.SellStartDate).TotalDays)); } }
+    }
+}
+
+
